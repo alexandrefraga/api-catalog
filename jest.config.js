10224b61b4eb6fc220a59deb5cce65e0b692fb9b
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2020-12-21 22:11:44 -0300

    ci: add path in tsconfig and jestconfig

--- jest.config.js
+++ jest.config.js
@@ -12,13 +12,19 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    '<rootDir>/src'
+    '<rootDir>/tests'
   ],
   // The test environment that will be used for testing
   testEnvironment: 'node',
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 
   // All imported modules in your tests should be mocked automatically
---
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -0,0 +1,25 @@
+import { SignUpController } from '@/presentation/controllers/signup-controller'
+
+type SutTypes = {
+  sut: SignUpController
+}
+const makeSut = (): SutTypes => {
+  const sut = new SignUpController()
+  return {
+    sut
+  }
+}
+describe('SignUpController', () => {
+  test('Should return 400 if no name is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.execute(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
