c99d37115f8dd345f4c50295dab39b4aa13a6a39
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-01-19 20:45:09 -0300

    refactor: add http helper

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,13 +1,11 @@
 import { Controller, SignUpRequestParameters, HttpResponse } from '../protocolls'
 import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignUpController implements Controller<SignUpRequestParameters> {
   async execute (request: SignUpRequestParameters): Promise<HttpResponse> {
     if (!request.name) {
-      return Promise.resolve({
-        statusCode: 400,
-        body: new MissingParamError('name')
-      })
+      return badRequest(new MissingParamError('name'))
     }
   }
 }
