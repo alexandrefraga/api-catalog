a1b420260ffc62fa832bccc81bad21d06519cacb
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-02 23:16:57 -0300

    feat: connect to mongodb befare app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
+import 'module-alias/register'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import env from '@/main/config/env'
 
-app.listen(5050, () => console.log('Server running...'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log('Server running...'))
+  })
+  .catch(console.error)
