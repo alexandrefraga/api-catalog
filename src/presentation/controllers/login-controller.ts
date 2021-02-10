da6c6104a71bca0670a3490a6a28ab436caa112f
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-09 22:12:34 -0300

    feat: ensure LoginController return 500 if Validator throws

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,14 +1,18 @@
 import { Controller, HttpResponse, LoginRequestParameters, Validation } from '@/presentation/protocolls'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 
 export class LoginController implements Controller<LoginRequestParameters> {
   constructor (private readonly validator: Validation) {}
 
   async execute (data: LoginRequestParameters): Promise<HttpResponse> {
-    const error = await this.validator.validate(data)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = await this.validator.validate(data)
+      if (error) {
+        return badRequest(error)
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
