5d908a6af564a9e81c825b5ba22b4f2a6482f80f
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-14 17:53:47 -0300

    feat: ensure DbAuthentication calls HasherComparer if LoadAccountByEmail return an account

--- src/data/usecases/db-athentication.ts
+++ src/data/usecases/db-athentication.ts
@@ -1,13 +1,18 @@
 import { Authentication, AuthenticationParameters, AuthenticationResponse } from '@/domain/usecases/authentication'
 import { LoadAccountByEmailRepository } from '@/domain/usecases/load-account'
+import { HasherComparer } from '../protocols/criptography/hasher-compare'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hasherComparer: HasherComparer
   ) {}
 
   async auth (data: AuthenticationParameters): Promise<AuthenticationResponse> {
-    await this.loadAccountByEmailRepository.loadByEmail(data.email)
+    const account = await this.loadAccountByEmailRepository.loadByEmail(data.email)
+    if (account) {
+      await this.hasherComparer.compare(data.email, account.email)
+    }
     return null
   }
 }
--- tests/data/mocks/mock-hasher.ts
+++ tests/data/mocks/mock-hasher.ts
@@ -1,4 +1,5 @@
 import { Hasher } from '@/data/protocols/criptography/hasher'
+import { HasherComparer } from '../protocols/criptography/hasher-compare'
 
 export const mockHasher = (): Hasher => {
   class Hasher implements Hasher {
@@ -7,4 +8,13 @@ export const mockHasher = (): Hasher => {
     }
   }
   return new Hasher()
+}
+
+export const mockHasherComparer = (): HasherComparer => {
+  class HasherComparerStub implements HasherComparer {
+    async compare (value: string, hash: string): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+  }
+  return new HasherComparerStub()
 }
