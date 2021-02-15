0b517bf8c3988a7e653a1b8c5b6948954761586c
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-15 16:33:50 -0300

    test: add login route

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,8 +1,9 @@
 import app from '@/main/config/app'
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
-import { fakeSignUpRequestParams } from '../mocks/mock-request'
+import { fakeLoginRequestParams, fakeSignUpRequestParams } from '../mocks/mock-request'
 import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 let accountCollection: Collection
 describe('Login Routes', () => {
@@ -24,6 +25,21 @@ describe('Login Routes', () => {
       await request(app)
         .post('/api/signup')
         .send(fakeSignUpRequestParams())
+        .expect(200)
+    })
+  })
+
+  describe('/login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('any_value', 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send(fakeLoginRequestParams())
         .expect(200)
     })
   })
