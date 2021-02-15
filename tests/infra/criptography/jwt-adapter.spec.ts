0fb2525dc0437a47c207c45e5b93190dc27c1e32
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-15 11:08:33 -0300

    test: ensure JwtAdapter throws if Jwt throw

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -18,6 +18,13 @@ describe('Jwt Adapter', () => {
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
   })
 
+  test('Should JwtAdapter throw if Jwt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrowError()
+  })
+
   test('Should return a token if sign success', async () => {
     const sut = makeSut()
     const token = await sut.encrypt('any_value')
