7da057789c451ce53fcdcadd999e6d1c6d8a5cc3
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-26 21:32:47 -0300

    test: ensure ValidateAccount Controller return 500 if validator throws

--- tests/presentation/controllers/validate-account-controller.spec.ts
+++ tests/presentation/controllers/validate-account-controller.spec.ts
@@ -1,6 +1,7 @@
 import { ValidateAccountController } from '@/presentation/controllers/validate-account-controller'
+import { ServerError } from '@/presentation/errors'
 import { mockValidateAccountParams, mockValidator } from '../mocks'
-import { Validation } from '../protocolls'
+import { Validation } from '@/presentation/protocolls'
 
 const validateParams = mockValidateAccountParams()
 
@@ -30,5 +31,13 @@ describe('ValidateAccount Controller', () => {
     const response = await sut.execute(validateParams)
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new Error('specific error'))
+  })
+
+  test('Should return 500 if Validator throws', async () => {
+    const { sut, validatorStub } = makeSut()
+    jest.spyOn(validatorStub, 'validate').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse = await sut.execute(validateParams)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError(''))
   })
 })
