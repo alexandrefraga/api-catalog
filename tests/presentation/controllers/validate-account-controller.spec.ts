c2a99c196d5cdc94567ab043986bb96e5356608b
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-26 21:18:07 -0300

    test: ensure ValidateAccount Controller return 400 if validator on error

--- tests/presentation/controllers/validate-account-controller.spec.ts
+++ tests/presentation/controllers/validate-account-controller.spec.ts
@@ -23,4 +23,12 @@ describe('ValidateAccount Controller', () => {
     await sut.execute(validateParams)
     expect(validateSpy).toHaveBeenCalledWith(validateParams.tokenValidation)
   })
+
+  test('Should return 400 if Validator return an error', async () => {
+    const { sut, validatorStub } = makeSut()
+    jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(Promise.resolve(new Error('specific error')))
+    const response = await sut.execute(validateParams)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new Error('specific error'))
+  })
 })
