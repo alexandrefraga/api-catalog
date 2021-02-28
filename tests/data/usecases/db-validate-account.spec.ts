41e634c55cc69f3888eafb1498e3ae119198a68c
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-02-28 20:50:31 -0300

    test: ensure DbValidateAccount throw if DdUpdateEmailRepository throws

--- tests/data/usecases/db-validate-account.spec.ts
+++ tests/data/usecases/db-validate-account.spec.ts
@@ -7,16 +7,16 @@ import { UpdateEmailRepository } from '../protocols/db/update-email-repository'
 type SutTypes = {
   sut: ValidateAccount
   jwtAdapterStub: Decrypter
-  dbAccountRepositoryStub: UpdateEmailRepository
+  dbUpdateEmailRepositoryStub: UpdateEmailRepository
 }
 const makeSut = (): SutTypes => {
   const jwtAdapterStub = mockDecrypter({ email: 'any_email' })
-  const dbAccountRepositoryStub = mockUpdateEmailRepository()
-  const sut = new DbValidateAccount(jwtAdapterStub, dbAccountRepositoryStub)
+  const dbUpdateEmailRepositoryStub = mockUpdateEmailRepository()
+  const sut = new DbValidateAccount(jwtAdapterStub, dbUpdateEmailRepositoryStub)
   return {
     sut,
     jwtAdapterStub,
-    dbAccountRepositoryStub
+    dbUpdateEmailRepositoryStub
   }
 }
 describe('DbValidateAccount', () => {
@@ -27,31 +27,38 @@ describe('DbValidateAccount', () => {
     expect(verifySpy).toHaveBeenCalledWith('any_token')
   })
 
-  test('Should DbAddAccount null if Decrypter throws', async () => {
+  test('Should DbValidateAccount null if Decrypter throws', async () => {
     const { sut, jwtAdapterStub } = makeSut()
     jest.spyOn(jwtAdapterStub, 'decrypt').mockImplementationOnce(() => { throw new Error() })
     const response = await sut.validate('any_token')
     expect(response).toBeNull()
   })
 
-  test('Should DbAddAccount null if Decrypter null', async () => {
+  test('Should DbValidateAccount null if Decrypter null', async () => {
     const { sut, jwtAdapterStub } = makeSut()
     jest.spyOn(jwtAdapterStub, 'decrypt').mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.validate('any_token')
     expect(response).toBeNull()
   })
 
-  test('Should DbAddAccount return null if token does not contain email', async () => {
+  test('Should DbValidateAccount return null if token does not contain email', async () => {
     const { sut, jwtAdapterStub } = makeSut()
     jest.spyOn(jwtAdapterStub, 'decrypt').mockReturnValueOnce(Promise.resolve({ field: 'any' }))
     const response = await sut.validate('any_token')
     expect(response).toBeNull()
   })
 
-  test('Should call DdAccountRepository with correct values', async () => {
-    const { sut, dbAccountRepositoryStub } = makeSut()
-    const updateEmailSpy = jest.spyOn(dbAccountRepositoryStub, 'updateEmail')
+  test('Should call DdUpdateEmailRepository with correct values', async () => {
+    const { sut, dbUpdateEmailRepositoryStub } = makeSut()
+    const updateEmailSpy = jest.spyOn(dbUpdateEmailRepositoryStub, 'updateEmail')
     await sut.validate('any_token')
     expect(updateEmailSpy).toHaveBeenCalledWith('any_email', true)
+  })
+
+  test('Should DbValidateAccount throw if DdUpdateEmailRepository throws', async () => {
+    const { sut, dbUpdateEmailRepositoryStub } = makeSut()
+    jest.spyOn(dbUpdateEmailRepositoryStub, 'updateEmail').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.validate('any_token')
+    await expect(promise).rejects.toThrow()
   })
 })
