7ad357c8d94b62c4220daa6218dcfa6e6b6b9848
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-01 22:08:32 -0300

    feat: ensure JwtAdapter return a decrypted data if verify on success

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,11 +1,16 @@
-import { Encrypter } from '@/data/protocols/criptography'
+import { Encrypter, Decrypter } from '@/data/protocols/criptography'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (jsonParams: string): Promise<string> {
     const token = await jwt.sign(jsonParams, this.secret)
     return token
+  }
+
+  async decrypt (token: string): Promise<string> {
+    const value: any = await jwt.verify(token, this.secret)
+    return value
   }
 }
