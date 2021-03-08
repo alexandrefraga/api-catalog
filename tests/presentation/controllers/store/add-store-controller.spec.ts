9d03867c6ba171470e6d844680c7992d51beb884
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-08 20:37:25 -0300

    test: ensure AddStoreController return 500 if Validator throws

--- tests/presentation/controllers/store/add-store-controller.spec.ts
+++ tests/presentation/controllers/store/add-store-controller.spec.ts
@@ -1,4 +1,5 @@
 import { AddStoreController } from '@/presentation/controllers/store/add-store-controller'
+import { ServerError } from '@/presentation/errors'
 import { Validation } from '@/presentation/protocolls'
 import { mockValidator, mockAddStoreParams } from '../../mocks'
 
@@ -29,5 +30,13 @@ describe('AddStore Controller', () => {
     const response = await sut.execute(mockAddStoreParams())
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new Error('specific error'))
+  })
+
+  test('Should return 500 if Validator throws', async () => {
+    const { sut, validatorStub } = makeSut()
+    jest.spyOn(validatorStub, 'validate').mockImplementationOnce(() => { throw new Error() })
+    const response = await sut.execute(mockAddStoreParams())
+    expect(response.statusCode).toBe(500)
+    expect(response.body).toEqual(new ServerError(''))
   })
 })
