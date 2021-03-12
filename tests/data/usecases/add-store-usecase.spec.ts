4a55f3d45b29c81b73a1f2bc53276c4516852870
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-12 20:50:24 -0300

    test: ensure return null if LoadStoreByDataRepository return a store

--- tests/data/usecases/add-store-usecase.spec.ts
+++ tests/data/usecases/add-store-usecase.spec.ts
@@ -1,6 +1,6 @@
 import { AddStoreUseCase } from '@/data/usecases/add-store-usecase'
 import { AddStore } from '@/domain/usecases/add-store'
-import { mockAddStoreParams, mockLoadStoreByDataRepository } from '../../mocks'
+import { mockAddStoreParams, mockLoadStoreByDataRepository, mockStoreModel } from '../../mocks'
 import { LoadStoreByDataRepository } from '../protocols/db/load-store-repository'
 
 type SutTypes = {
@@ -29,5 +29,12 @@ describe('AddStore UseCase', () => {
         city: 'any_city'
       }
     })
+  })
+
+  test('Should return null if loadStoreByDataRepository return a store', async () => {
+    const { sut, loadStoreByDataRepositoryStub } = makeSut()
+    jest.spyOn(loadStoreByDataRepositoryStub, 'loadStoreByData').mockReturnValueOnce(Promise.resolve(mockStoreModel()))
+    const response = await sut.add(mockAddStoreParams())
+    expect(response).toBeNull()
   })
 })
