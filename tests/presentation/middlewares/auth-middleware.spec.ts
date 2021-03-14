01f6e8831bf95a88161d90832aaec5066d8e8bad
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-14 19:35:22 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken if correct token

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -2,14 +2,19 @@ import { Middleware } from '@/presentation/protocolls'
 import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware'
 import { forbidden } from '@/presentation/helpers/http-helper'
 import { AccessDeniedError } from '@/presentation/errors/access-denied-error'
+import { LoadAccountByToken } from '@/domain/usecases/load-account-by-Token'
+import { mockLoadAccountByToken } from '../../mocks'
 
 type SutTypes = {
   sut: Middleware
+  loadAccountByTokenStub: LoadAccountByToken
 }
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
+  const loadAccountByTokenStub = mockLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
   return {
-    sut
+    sut,
+    loadAccountByTokenStub
   }
 }
 describe('Auth Middleware', () => {
@@ -17,5 +22,17 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const response = await sut.execute({})
     expect(response).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken if x-access-token exists in headers', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadByTokenSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const request = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    await sut.execute(request)
+    expect(loadByTokenSpy).toHaveBeenCalledWith('any_token')
   })
 })
