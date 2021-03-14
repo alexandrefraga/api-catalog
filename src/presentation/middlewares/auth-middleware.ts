4ced8b5431d6966b8df3e49d027550124efdb89d
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-14 20:08:41 -0300

    feat: ensure AuthMiddleware return 200 if LoadAccountByToken return an account

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { LoadAccountByToken } from '@/domain/usecases/load-account-by-Token'
 import { AccessDeniedError } from '../errors'
-import { forbidden } from '../helpers/http-helper'
+import { forbidden, success } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocolls'
 import { Middleware } from '../protocolls/middleware'
 
@@ -12,7 +12,10 @@ export class AuthMiddleware implements Middleware {
   async execute (request: HttpRequest): Promise<HttpResponse> {
     const token = request.headers?.['x-access-token']
     if (token) {
-      await this.loadAccountByToken.load(token)
+      const account = await this.loadAccountByToken.load(token)
+      if (account) {
+        return success({ userId: account.id })
+      }
     }
     return forbidden(new AccessDeniedError())
   }
