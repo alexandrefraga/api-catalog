574ca6a95aea174ea6271cb7546eafe7bafe0f3b
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-03-14 09:50:17 -0300

    test: ensure StoreMongoRepository return a store if add on success

--- tests/infra/db/mongodb/store-repository.spec.ts
+++ tests/infra/db/mongodb/store-repository.spec.ts
@@ -4,6 +4,8 @@ import { Collection } from 'mongodb'
 import MockDate from 'mockdate'
 import { mockAddStoreParams, mockInsertStoreParams } from '@/../tests/mocks'
 
+const addStoreParams = mockAddStoreParams()
+
 const makeSut = (): StoreMongoRepository => {
   return new StoreMongoRepository()
 }
@@ -28,19 +30,36 @@ describe('Store Mongo Repository', () => {
   describe('LoadByData', () => {
     test('Should return null if loadByData fail', async () => {
       const sut = makeSut()
-      const { company, tradingName, address } = mockAddStoreParams()
-      const store = await sut.loadByData({ company, tradingName, address })
+      const store = await sut.loadByData({
+        company: addStoreParams.company,
+        tradingName: addStoreParams.tradingName,
+        address: addStoreParams.address
+      })
       expect(store).toBeFalsy()
     })
 
     test('Should return a store if loadByData on success', async () => {
       await storeCollection.insertOne(mockInsertStoreParams())
       const sut = makeSut()
-      const { company, tradingName, address } = mockAddStoreParams()
-      const store = await sut.loadByData({ company, tradingName, address })
+      const store = await sut.loadByData({
+        company: addStoreParams.company,
+        tradingName: addStoreParams.tradingName,
+        address: addStoreParams.address
+      })
       expect(store).toBeTruthy()
-      expect(store.company).toBe(company)
-      expect(store.address).toEqual(address)
+      expect(store.company).toBe(addStoreParams.company)
+      expect(store.address).toEqual(addStoreParams.address)
+    })
+  })
+
+  describe('Add', () => {
+    test('Should return a store if add on success', async () => {
+      const sut = makeSut()
+      const store = await sut.add(addStoreParams)
+      expect(store).toBeTruthy()
+      expect(store.company).toBe(addStoreParams.company)
+      expect(store.address).toEqual(addStoreParams.address)
+      expect(store.usersAdmin).toContain(addStoreParams.userId)
     })
   })
 })
