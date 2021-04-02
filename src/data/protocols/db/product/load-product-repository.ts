ab29d5e3d3c187f9ebb2fcfde9fe8f90f3714676
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-02 19:49:04 -0300

    feat: ensure AddProductUseCase calls LoadProductByDataRepository with correct values

---
+++ src/data/protocols/db/product/load-product-repository.ts
@@ -0,0 +1,11 @@
+import { ProductModel } from '@/domain/models/product-model'
+
+export type LoadProductByDataParams = {
+  trademark: string
+  reference: string
+  storeId: string
+}
+
+export interface LoadProductByDataRepository {
+  loadByData (data: LoadProductByDataParams): Promise<ProductModel>
+}
--- src/data/usecases/product/add-product-usecase.ts
+++ src/data/usecases/product/add-product-usecase.ts
@@ -2,10 +2,12 @@ import { LoadStoreByIdRepository } from '@/data/protocols/db'
 import { ProductModel } from '@/domain/models/product-model'
 import { AddProduct, AddProductUseCaseParams } from '@/domain/usecases/product/add-product'
 import { InvalidParamError } from '@/data/errors'
+import { LoadProductByDataRepository } from '@/data/protocols/db/product/load-product-repository'
 
 export class AddProductUseCase implements AddProduct {
   constructor (
-    private readonly loadStoreByIdRepository: LoadStoreByIdRepository
+    private readonly loadStoreByIdRepository: LoadStoreByIdRepository,
+    private readonly loadProductByDataRepository: LoadProductByDataRepository
   ) {}
 
   async add (data: AddProductUseCaseParams): Promise<ProductModel | Error> {
@@ -13,6 +15,8 @@ export class AddProductUseCase implements AddProduct {
     if (!store) {
       return new InvalidParamError('storeId')
     }
+    const { trademark, reference, storeId } = data
+    await this.loadProductByDataRepository.loadByData({ trademark, reference, storeId })
     return null
   }
 }
