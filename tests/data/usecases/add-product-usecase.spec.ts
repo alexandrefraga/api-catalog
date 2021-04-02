dff3593711d260a13774533ba98af50fb50d268b
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-02 11:07:23 -0300

    test: ensure AddProductUseCase return an error if LoadStoreByIdRepository fail

--- tests/data/usecases/add-product-usecase.spec.ts
+++ tests/data/usecases/add-product-usecase.spec.ts
@@ -2,6 +2,7 @@ import { mockAddProductUseCaseParams } from '../../mocks/mock-product'
 import { LoadStoreByIdRepository } from '@/data/protocols/db'
 import { AddProductUseCase } from '@/data/usecases/product/add-product-usecase'
 import { mockLoadStoreByIdRepository } from '../../mocks'
+import { InvalidParamError } from '@/data/errors'
 
 const params = mockAddProductUseCaseParams()
 
@@ -10,7 +11,7 @@ type SutTypes = {
   loadStoreByIdRepositoryStub: LoadStoreByIdRepository
 }
 const makeSut = (): SutTypes => {
-  const loadStoreByIdRepositoryStub = mockLoadStoreByIdRepository(false)
+  const loadStoreByIdRepositoryStub = mockLoadStoreByIdRepository(true)
   const sut = new AddProductUseCase(loadStoreByIdRepositoryStub)
   return {
     sut,
@@ -23,5 +24,19 @@ describe('AddProduct Usecase', () => {
     const loadByIdSpy = jest.spyOn(loadStoreByIdRepositoryStub, 'loadById')
     await sut.add(params)
     expect(loadByIdSpy).toHaveBeenCalledWith(params.storeId)
+  })
+
+  test('Should return an error if LoadStoreByIdRepository return null', async () => {
+    const { sut, loadStoreByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadStoreByIdRepositoryStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const response = await sut.add(params)
+    expect(response).toEqual(new InvalidParamError('storeId'))
+  })
+
+  test('Should AddProductUseCase throw if LoadStoreByIdRepository throws', async () => {
+    const { sut, loadStoreByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadStoreByIdRepositoryStub, 'loadById').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.add(params)
+    await expect(promise).rejects.toThrow()
   })
 })
