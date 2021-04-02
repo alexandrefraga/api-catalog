dc122fe544a241dab3b5a6b74fd2329bf60c5cf0
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-02 11:04:04 -0300

    feat: ensure AddProductUseCase return an error if LoadStoreByIdRepository fail

---
+++ src/data/errors/index.ts
@@ -0,0 +1,1 @@
+export * from './invali-param-error'
--- src/data/usecases/product/add-product-usecase.ts
+++ src/data/usecases/product/add-product-usecase.ts
@@ -1,6 +1,7 @@
 import { LoadStoreByIdRepository } from '@/data/protocols/db'
 import { ProductModel } from '@/domain/models/product-model'
 import { AddProduct, AddProductUseCaseParams } from '@/domain/usecases/product/add-product'
+import { InvalidParamError } from '@/data/errors'
 
 export class AddProductUseCase implements AddProduct {
   constructor (
@@ -8,7 +9,10 @@ export class AddProductUseCase implements AddProduct {
   ) {}
 
   async add (data: AddProductUseCaseParams): Promise<ProductModel | Error> {
-    await this.loadStoreByIdRepository.loadById(data.storeId)
-    return Promise.resolve(null)
+    const store = await this.loadStoreByIdRepository.loadById(data.storeId)
+    if (!store) {
+      return new InvalidParamError('storeId')
+    }
+    return null
   }
 }
