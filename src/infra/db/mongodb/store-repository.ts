f17d10e323d921ac69a5e17754d62a9c3de15af8
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-03 17:07:09 -0300

    feat: ensure LoadStoreByIdRepository return a store on success

--- src/infra/db/mongodb/store-repository.ts
+++ src/infra/db/mongodb/store-repository.ts
@@ -1,20 +1,24 @@
-import { AddStoreRepository, LoadStoreByDataParams, LoadStoreByDataRepository } from '@/data/protocols/db'
+import { AddStoreRepository, LoadStoreByDataParams, LoadStoreByDataRepository, LoadStoreByIdRepository } from '@/data/protocols/db'
 import { StoreModel } from '@/domain/models/store-model'
 import { AddStoreParams } from '@/domain/usecases/store/add-store'
 import { MongoHelper } from './mongo-helper'
 
-export class StoreMongoRepository implements LoadStoreByDataRepository, AddStoreRepository {
+export class StoreMongoRepository implements AddStoreRepository, LoadStoreByIdRepository, LoadStoreByDataRepository {
+  async add (data: AddStoreParams): Promise<StoreModel> {
+    const storeCollection = await MongoHelper.getCollection('stores')
+    const result = await storeCollection.insertOne(data)
+    return MongoHelper.map(result.ops[0])
+  }
+
+  async loadById (id: string): Promise<StoreModel> {
+    const storeCollection = await MongoHelper.getCollection('stores')
+    const store = await storeCollection.findOne({ id })
+    return store && MongoHelper.map(store)
+  }
+
   async loadByData (data: LoadStoreByDataParams): Promise<StoreModel> {
     const storeCollection = await MongoHelper.getCollection('stores')
     const store = await storeCollection.findOne(data)
     return store && MongoHelper.map(store)
-  }
-
-  async add (data: AddStoreParams): Promise<StoreModel> {
-    const storeModel = Object.assign({}, data, { usersAdmin: [data.userId] })
-    delete storeModel.userId
-    const storeCollection = await MongoHelper.getCollection('stores')
-    const result = await storeCollection.insertOne(storeModel)
-    return MongoHelper.map(result.ops[0])
   }
 }
