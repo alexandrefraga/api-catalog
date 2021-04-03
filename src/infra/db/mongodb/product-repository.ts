be2dc6bc6afbda735adf0a87b04b302e8bc2dc0a
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-02 22:29:15 -0300

    feat: ensure LoadProductByDataRepository return a product on success

--- src/infra/db/mongodb/product-repository.ts
+++ src/infra/db/mongodb/product-repository.ts
@@ -1,11 +1,17 @@
-import { AddProductRepository, AddProductRepositoryParams } from '@/data/protocols/db'
+import { AddProductRepository, AddProductRepositoryParams, LoadProductByDataParams, LoadProductByDataRepository } from '@/data/protocols/db'
 import { ProductModel } from '@/domain/models/product-model'
 import { MongoHelper } from './mongo-helper'
 
-export class ProductMongoRepository implements AddProductRepository {
+export class ProductMongoRepository implements AddProductRepository, LoadProductByDataRepository {
   async add (product: AddProductRepositoryParams): Promise<ProductModel> {
     const productCollection = await MongoHelper.getCollection('products')
     const result = await productCollection.insertOne(product)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByData (data: LoadProductByDataParams): Promise<ProductModel> {
+    const productCollection = await MongoHelper.getCollection('products')
+    const result = await productCollection.findOne(data)
+    return result && MongoHelper.map(result)
   }
 }
