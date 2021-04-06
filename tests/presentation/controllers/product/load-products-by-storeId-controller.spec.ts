e826c08371b76e15353d421ac370b9c038d2da62
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-04-05 21:10:43 -0300

    test: ensure LoadProductsByStoreIDController return 200 on success

--- tests/presentation/controllers/product/load-products-by-storeId-controller.spec.ts
+++ tests/presentation/controllers/product/load-products-by-storeId-controller.spec.ts
@@ -1,20 +1,24 @@
-import { mockValidator } from '@/../tests/mocks'
+import { mockLoadProductsByStoreIdUseCase, mockProductModel, mockValidator } from '@/../tests/mocks'
 import { ServerError } from '@/presentation/errors'
 import { Validation } from '@/presentation/protocolls'
 import { LoadProductsByStoreIDController } from '@/presentation/controllers/product/load-products-by-storeId-controller'
+import { InvalidParamError } from '@/data/errors'
 
-const request = { storeId: 'storeId' }
+const request = { storeId: 'valid_storeId' }
 
 type SutTypes = {
   sut: LoadProductsByStoreIDController
   validatorStub: Validation
+  loadProductsByStoreIdUseCase
 }
 const makeSut = (): SutTypes => {
   const validatorStub = mockValidator()
-  const sut = new LoadProductsByStoreIDController(validatorStub)
+  const loadProductsByStoreIdUseCase = mockLoadProductsByStoreIdUseCase()
+  const sut = new LoadProductsByStoreIDController(validatorStub, loadProductsByStoreIdUseCase)
   return {
     sut,
-    validatorStub
+    validatorStub,
+    loadProductsByStoreIdUseCase
   }
 }
 describe('LoadProductsByStoreID Controller', () => {
@@ -39,5 +43,35 @@ describe('LoadProductsByStoreID Controller', () => {
     const response = await sut.execute(request)
     expect(response.statusCode).toBe(500)
     expect(response.body).toEqual(new ServerError(''))
+  })
+
+  test('Should call LoadProductsByStoreIdUseCase with correct values', async () => {
+    const { sut, loadProductsByStoreIdUseCase } = makeSut()
+    const loadByStoreSpy = jest.spyOn(loadProductsByStoreIdUseCase, 'loadByStore')
+    await sut.execute(request)
+    expect(loadByStoreSpy).toHaveBeenCalledWith(request.storeId)
+  })
+
+  test('Should return 403 if LoadProductsByStoreIdUseCase return a specific error', async () => {
+    const { sut, loadProductsByStoreIdUseCase } = makeSut()
+    jest.spyOn(loadProductsByStoreIdUseCase, 'loadByStore').mockReturnValueOnce(Promise.resolve(new InvalidParamError('storeId')))
+    const response = await sut.execute(request)
+    expect(response.statusCode).toBe(403)
+    expect(response.body).toEqual(new InvalidParamError('storeId'))
+  })
+
+  test('Should return 500 if LoadProductsByStoreIdUseCase throws', async () => {
+    const { sut, loadProductsByStoreIdUseCase } = makeSut()
+    jest.spyOn(loadProductsByStoreIdUseCase, 'loadByStore').mockImplementationOnce(() => { throw new Error() })
+    const response = await sut.execute(request)
+    expect(response.statusCode).toBe(500)
+    expect(response.body).toEqual(new ServerError(''))
+  })
+
+  test('Should return 200 if LoadProductsByStoreIdUseCase return products', async () => {
+    const { sut } = makeSut()
+    const response = await sut.execute(request)
+    expect(response.statusCode).toBe(200)
+    expect(response.body[0]).toEqual(mockProductModel())
   })
 })
