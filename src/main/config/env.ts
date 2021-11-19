b8ad5b6d2291fa3862068fe778a75ad1d39b9dc0
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2021-11-18 22:57:37 -0300

    chore: add dotenv for development

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,15 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config()
+}
+
 export default {
   baseUrl: 'http://localhost:5050/api',
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/api-catalog',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'ab69gt94=hr',
-  mailFrom: 'Alexandre Fraga<alexandrenfraga@gmail.com>',
+  jwtSecret: process.env.JWT_SECRET,
+  mailFrom: process.env.MAIL_FROM,
   mailParams: {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT),
     auth: {
-      user: '664be95f8e43a4',
-      pass: 'caa9298ef4684e'
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD
     }
   }
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -5,6 +5,6 @@ import env from '@/main/config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log('Server running...'))
+    app.listen(env.port, () => console.log(`Server running... ${env.baseUrl}`))
   })
   .catch(console.error)
